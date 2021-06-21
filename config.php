9a36aeb590c35b062410c597f411a387aeafdba6
akiho-o <akiho.o.0930@gmail.com>
2021-06-22 02:01:02 +0900

    featureブランチで実装する機能

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DSN', 'mysql:host=db;dbname=customer_management;charset=utf8');
+define('USER', 'customer_admin');
+define('PASSWORD', '1234');
+
+// エラーメッセージ
+define('MSG_TITLE_REQUIRED', 'タスク名を入力してください');
+define('MSG_TITLE_NO_CHANGE', 'タスク名が変更されていません');
